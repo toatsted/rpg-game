3fd74ab430fcc0e3dd71a1df5de79b44b2257bcb
Sam <samueljacksonhicks@gmail.com>
2018-02-19 17:50:09 -0600

    still not functional, looks alright

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,33 @@
-$(document).ready(function(){
-	let player = null;
+$(document).ready(function() {
 
-	$(".character").on("click", function(){
+	function Character(hp, ap, cap){
+		this.hp = hp;
+		this.ap = ap;
+		this.cap = cap;
+	}
+
+	let player = null;
+	let enemy = null;
+
+	$(".character").on("click", function() {
+
 		let char = $(this);
 		placehold = char.detach();
 
-		if(char.parent("#idle")){
-			if(player === null){
+		if (char.parent("#idle")) {
+			if (player === null) {
+
 				player = char;
 				$("#player").append(placehold);
-			}else{
+			} else if (enemy === null){
+
+				enemy = char;
 				$("#defense").append(placehold);
+
+				$("#playerText").html("player <span><button id='attack'>attack</button></span>");
+			} else {
+				$("#idle").append(placehold);
 			}
 		}
-
 	});
 });
