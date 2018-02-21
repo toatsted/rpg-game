ce142bc0bdb575ef786529778eb466aa0e44d81d
Sam <samueljacksonhicks@gmail.com>
2018-02-20 18:05:23 -0600

    game is done. need theme and balancing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,27 +5,34 @@ $(document).ready(function() {
 		this.health = health;
 		this.attack = attack;
 		this.cAttack = cAttack;
+		this.baseAttack = attack;
 	}
 
 	// object to hold the cards
 	let cards = {
-		one: new Card(200, 16, 24),
+		one: new Card(200, 18, 34),
 
-		two: new Card(180, 19, 18),
+		two: new Card(180, 19, 28),
 
-		three: new Card(220, 13, 20),
+		three: new Card(220, 17, 30),
 
-		four: new Card(200, 20, 16),
+		four: new Card(200, 20, 26),
 	}
 
 	// hold the jQuery objects of the card
 	playerCard = null;
 	defenderCard = null;
 
+	// hold the Card objects
+	playerObj = null;
+	defenderObj = null;
+
 	// hold the jQuery objects of the zones
 	idleArea = $("#idle");
 	playerArea = $("#player");
 	defenderArea = $("#defense");
+
+	infoLine = $("#infoLine");
 
 	// click on an idle character to select
 	$(".idle").on("click", function() {
@@ -42,7 +49,26 @@ $(document).ready(function() {
 			playerCard.detach();
 			playerArea.prepend(playerCard);
 
-			$("#infoLine").text("choose a card to fight")
+			infoLine.text("choose a card to fight")
+
+			switch (parseInt(playerCard.attr("id"))) {
+				case 1:
+					playerObj = cards.one;
+					break;
+
+				case 2:
+					playerObj = cards.two;
+					break;
+
+				case 3:
+					playerObj = cards.three;
+					break;
+
+				case 4:
+					playerObj = cards.four;
+					break;
+
+			}
 
 		} else if (defenderCard === null) {
 
@@ -57,12 +83,72 @@ $(document).ready(function() {
 			$("#buttonLine")
 				.html("player <span><button id='attackButton'>attack</button></span>");
 
-			$("#infoLine").html("<strong>FIGHT!</strong>");
+			infoLine.html("<strong>FIGHT!</strong>");
+
+			switch (parseInt(defenderCard.attr("id"))) {
+				case 1:
+					defenderObj = cards.one;
+					break;
+
+				case 2:
+					defenderObj = cards.two;
+					break;
+
+				case 3:
+					defenderObj = cards.three;
+					break;
+
+				case 4:
+					defenderObj = cards.four;
+					break;
+
+			}
 		}
 	});
 
 	// click the attack button to attack
 	$("#buttonLine").on("click", "#attackButton", function() {
+
+		defenderObj.health -= playerObj.attack;
+		playerObj.health -= defenderObj.cAttack;
+
+		playerObj.attack += playerObj.baseAttack;
+
+		playerCard
+			.find(".hp")
+			.text(playerObj.health);
+
+		defenderCard
+			.find(".hp")
+			.text(defenderObj.health);
+
+
+		if(defenderObj.health <= 0){
+
+			defenderCard.detach();
+			defenderCard = null;
+			defenderObj = null;
+
+			infoLine.text("select a new character to fight");
+
+			if(idleArea.children().length === 1){
+				alert("You win!");
+
+				$("#buttonLine")
+				.html("player <span><button id='resetButton'>reset</button></span>");
+			}
+
+		}else if(playerObj.health <= 0){
+
+			alert("You Lost!");
+
+			$("#buttonLine")
+			.html("player <span><button id='resetButton'>reset</button></span>");
+
+		}
+	});
+
+	$("#buttonLine").on("click", "#resetButton", function(){
 
 	});
 
